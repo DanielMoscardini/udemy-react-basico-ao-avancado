1e8aceb209a0ac4bf75057dcae551171ca690ecf
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2023-01-19 20:41:44 -0300

    Finalizando secao 4 - react com css

---
+++ 04_css_react/css_react/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MyComponent.css';
+
+const MyComponent = () => {
+  return (
+    <div className="my-component">
+      <h1>CSS de componente</h1>
+      <p>Este é o paragrafo do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
