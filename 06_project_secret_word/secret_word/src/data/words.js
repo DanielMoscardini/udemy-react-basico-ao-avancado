05323e14a61a9e2d5d0f60a8031e1eb6d753cda2
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2023-01-23 18:55:12 -0300

    Inicializando projeto

---
+++ 06_project_secret_word/secret_word/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ['Motor', 'Porta', 'Pneu', 'Antena'],
+  fruta: ['Banana', 'Uva', 'Laranja', 'Kiwi'],
+  corpo: ['Perna', 'Orelha', 'Osso', 'Olho'],
+  computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete'],
+  programacao: ['Linguagem', 'Framework', 'JavaScript', 'React'],
+  alimento: ['Arroz', 'Carne', 'Leite', 'Ovo'],
+};
