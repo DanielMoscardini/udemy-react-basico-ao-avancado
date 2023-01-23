47bd4d3ace78a513f77b97553c44c781f6b57033
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2023-01-22 23:01:30 -0300

    Criando formulario

--- 05_react_formulario/forms/src/App.js
+++ 05_react_formulario/forms/src/App.js
@@ -1,7 +1,16 @@
+// Arquivo App.js
+
 import './App.css';
 
+import MyForm from './components/MyForm';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Forms</h2>
+      <MyForm />
+    </div>
+  );
 }
 
 export default App;
