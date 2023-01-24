91fc1532e7a9fa1f083bd7501d441da7f24e8b73
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2023-01-24 00:29:14 -0300

    Adicionando

--- 06_project_secret_word/secret_word/src/components/Game.js
+++ 06_project_secret_word/secret_word/src/components/Game.js
@@ -4,9 +4,31 @@ import React from 'react';
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="point">
+        <span>Pontuacao: 000</span>
+      </p>
+      <h1>Advinhe a palavra</h1>
+      <h3 className="tip">
+        Dica sobre a palavra <span>Dica...</span>
+      </h3>
+      <p>Voce ainda tem x tentativas</p>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras ja utilizadas: </p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   );
 };
